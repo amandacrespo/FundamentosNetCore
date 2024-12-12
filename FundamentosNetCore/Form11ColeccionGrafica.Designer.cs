a4dda287f7408c8a5229d4641c8726c5c9af1815
amandacrespo <mancreslu@hotmail.com>
2024-12-12 11:48:14 +0100

    Clase 12/12

---
+++ FundamentosNetCore/Form11ColeccionGrafica.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace FundamentosNetCore
+{
+    partial class Form11ColeccionGrafica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstElementos = new ListBox();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            btnInsert = new Button();
+            btnDelet = new Button();
+            btnBorrar = new Button();
+            lblItemSeleccionado = new Label();
+            lblIndexSeleccionado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(78, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Elementos";
+            // 
+            // lstElementos
+            // 
+            lstElementos.FormattingEnabled = true;
+            lstElementos.Location = new Point(12, 32);
+            lstElementos.Name = "lstElementos";
+            lstElementos.Size = new Size(231, 264);
+            lstElementos.TabIndex = 1;
+            lstElementos.SelectedIndexChanged += lstElementos_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(286, 9);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 20);
+            label2.TabIndex = 2;
+            label2.Text = "Nuevo elemento";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(286, 32);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(184, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // btnInsert
+            // 
+            btnInsert.Location = new Point(286, 65);
+            btnInsert.Name = "btnInsert";
+            btnInsert.Size = new Size(94, 29);
+            btnInsert.TabIndex = 4;
+            btnInsert.Text = "Insertar";
+            btnInsert.UseVisualStyleBackColor = true;
+            btnInsert.Click += btnInsert_Click;
+            // 
+            // btnDelet
+            // 
+            btnDelet.Location = new Point(286, 100);
+            btnDelet.Name = "btnDelet";
+            btnDelet.Size = new Size(94, 29);
+            btnDelet.TabIndex = 5;
+            btnDelet.Text = "Eliminar";
+            btnDelet.UseVisualStyleBackColor = true;
+            btnDelet.Click += btnDelet_Click;
+            // 
+            // btnBorrar
+            // 
+            btnBorrar.Location = new Point(286, 135);
+            btnBorrar.Name = "btnBorrar";
+            btnBorrar.Size = new Size(94, 29);
+            btnBorrar.TabIndex = 6;
+            btnBorrar.Text = "Borrar todo";
+            btnBorrar.UseVisualStyleBackColor = true;
+            btnBorrar.Click += btnBorrar_Click;
+            // 
+            // lblItemSeleccionado
+            // 
+            lblItemSeleccionado.AutoSize = true;
+            lblItemSeleccionado.Location = new Point(12, 326);
+            lblItemSeleccionado.Name = "lblItemSeleccionado";
+            lblItemSeleccionado.Size = new Size(50, 20);
+            lblItemSeleccionado.TabIndex = 7;
+            lblItemSeleccionado.Text = "label3";
+            // 
+            // lblIndexSeleccionado
+            // 
+            lblIndexSeleccionado.AutoSize = true;
+            lblIndexSeleccionado.Location = new Point(12, 360);
+            lblIndexSeleccionado.Name = "lblIndexSeleccionado";
+            lblIndexSeleccionado.Size = new Size(50, 20);
+            lblIndexSeleccionado.TabIndex = 8;
+            lblIndexSeleccionado.Text = "label4";
+            // 
+            // Form11ColeccionGrafica
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(479, 411);
+            Controls.Add(lblIndexSeleccionado);
+            Controls.Add(lblItemSeleccionado);
+            Controls.Add(btnBorrar);
+            Controls.Add(btnDelet);
+            Controls.Add(btnInsert);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(lstElementos);
+            Controls.Add(label1);
+            Name = "Form11ColeccionGrafica";
+            Text = "Form11ColeccionGrafica";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstElementos;
+        private Label label2;
+        private TextBox textBox1;
+        private Button btnInsert;
+        private Button btnDelet;
+        private Button btnBorrar;
+        private Label lblItemSeleccionado;
+        private Label lblIndexSeleccionado;
+    }
+}
---
+++ FundamentosNetCore/Form11ColeccionGrafica.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FundamentosNetCore
+{
+    public partial class Form11ColeccionGrafica : Form
+    {
+        public Form11ColeccionGrafica()
+        {
+            InitializeComponent();
+        }
+
+        private void btnInsert_Click(object sender, EventArgs e)
+        {
+            string elem = this.textBox1.Text;
+            this.lstElementos.Items.Add(elem);
+        }
+
+        private void btnDelet_Click(object sender, EventArgs e)
+        {
+            //string elem = this.lstElementos.SelectedItems.ToString();
+            //this.lstElementos.Items.Remove(elem);
+            int i = this.lstElementos.SelectedIndex;
+            this.lstElementos.Items.RemoveAt(i);
+
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            this.lstElementos.Items.Clear();
+        }
+
+        private void lstElementos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.lstElementos.SelectedIndex != -1)
+            {
+                this.lblIndexSeleccionado.Text = "Index: " + this.lstElementos.SelectedIndex;
+                this.lblItemSeleccionado.Text = "Item: " + this.lstElementos.SelectedItem;
+            }
+        }
+    }
+}
